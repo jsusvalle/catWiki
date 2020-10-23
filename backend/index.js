ea02a5dd18c025f9f554031f0f007d831f6f278d
jsusvalle <jesusvalle2001@hotmail.com>
2020-10-23 17:54:48 -0500

    fix dependencies

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 require('dotenv').config({path: `.env.${NODE_ENV}`});
 
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const apiRoutes = require('./routes/catsBreads');
 
